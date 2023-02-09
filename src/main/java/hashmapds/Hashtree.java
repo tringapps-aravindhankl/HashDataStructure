a333434a203f59f571582682dd7863cd4c45bdcd
tringapps-aravindhankl <123533867+tringapps-aravindhankl@users.noreply.github.com>
2023-02-09 20:52:51 +0530

    Update Hashtree.java

--- src/main/java/hashmapds/Hashtree.java
+++ src/main/java/hashmapds/Hashtree.java
@@ -5,7 +5,7 @@ import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Treeset {
+public class Hashtree {
 	public static void main(String []args) {
 		TreeSet<Integer> ts=new TreeSet<>();
 		Logger l=Logger.getLogger(" ");
